9807a6c030712e5cae928240b3f95a6677a3b8e1
aleksgov <aleksandrgovo@gmail.com>
2024-06-30 15:50:45 +0300

    design

--- main.py
+++ main.py
@@ -1,1 +1,18 @@
-print("hELLO")
+from PyQt5 import QtWidgets, uic
+
+# Импортируйте ваш сгенерированный класс
+from design import Ui_MainWindow
+
+class MyApp(QtWidgets.QMainWindow):
+    def __init__(self):
+        super().__init__()
+
+        self.ui = Ui_MainWindow()
+        self.ui.setupUi(self)
+
+if __name__ == "__main__":
+    import sys
+    app = QtWidgets.QApplication(sys.argv)
+    window = MyApp()
+    window.show()
+    sys.exit(app.exec_())
